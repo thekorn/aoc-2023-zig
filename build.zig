50418dea88707f7da4e43e00fef1f73066521cd6
Markus Korn <markus.korn@gmail.com>
2023-12-03 17:56:35 +0100

    feat: add GitHub Actions workflow for build and test feat: add lint job to GitHub Actions workflow feat(build.zig): add setup for unit tests feat: add new test file 'tests.zig' for unit testing

--- build.zig
+++ build.zig
@@ -83,4 +83,21 @@ pub fn build(b: *Build) void {
         linkObject(b, test_cmd);
         test_util.dependOn(&test_cmd.step);
     }
+
+    // Set up all tests
+    {
+        const unit_tests = b.addTest(.{
+            .root_source_file = .{ .path = "src/tests.zig" },
+            .target = target,
+            .optimize = mode,
+        });
+
+        const run_unit_tests = b.addRunArtifact(unit_tests);
+
+        // Similar to creating the run step earlier, this exposes a `test` step to
+        // the `zig build --help` menu, providing a way for the user to request
+        // running the unit tests.
+        const test_step = b.step("test", "Run unit tests");
+        test_step.dependOn(&run_unit_tests.step);
+    }
 }
---
+++ src/tests.zig
@@ -0,0 +1,6 @@
+test {
+    _ = @import("utils.zig");
+    _ = @import("day01.zig");
+    _ = @import("day02.zig");
+    _ = @import("day03.zig");
+}
