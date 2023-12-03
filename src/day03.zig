ddedace400b3c58083c3b014c72468bb12e6fab4
Markus Korn <markus.korn@gmail.com>
2023-12-03 12:58:18 +0100

    feat(day03.zig): add solution for day 3 challenge
    This commit includes the solution for the day 3 challenge in the Zig programming language. The solution includes two functions, solve1 and solve2, which are used to solve the two parts of the challenge. The main function is used to run the solutions and print the results. Two tests are also included to verify the correctness of the solutions.

---
+++ src/day03.zig
@@ -0,0 +1,154 @@
+const std = @import("std");
+const utils = @import("utils.zig");
+
+fn getNumber(content: []const u8, i: *usize) i32 {
+    var result: i32 = 0;
+    var pos: usize = 0;
+    while (pos < content.len) : (pos += 1) {
+        if (!std.ascii.isDigit(content[pos])) {
+            break;
+        }
+        result = (result * 10) + (content[pos] - '0');
+    }
+    i.* -= pos;
+    return result;
+}
+
+fn isSymbol(c: []const u8) bool {
+    return !std.ascii.isDigit(c[0]) and !std.mem.eql(u8, c, ".") and !std.mem.eql(u8, c, "\n");
+}
+
+fn getHasSymbol(content: []const u8, start: usize, stop: usize, numCols: usize) bool {
+    //utils.print(">>>> start: {}, stop: {}", .{ start, stop });
+    for (start..stop) |i| {
+        //utils.print("\n>>>> {}\n", .{i});
+        // left char
+        if (i > 0) {
+            if (isSymbol(content[i - 1 .. i])) {
+                //utils.print("______________________left: {}\n", .{content[i - 1]});
+                return true;
+            }
+        }
+        // right char
+        if (i < content.len - 1) {
+            if (isSymbol(content[i + 1 .. i + 2])) {
+                //utils.print("______________________right: {}\n", .{content[i + 1]});
+                return true;
+            }
+        }
+        // top char
+        if (i > numCols) {
+            if (isSymbol(content[i - numCols .. i - numCols + 1])) {
+                //utils.print("______________________top: {}\n", .{content[i - numCols]});
+                return true;
+            }
+        }
+        // bottom char
+        if (i < content.len - numCols) {
+            if (isSymbol(content[i + numCols .. i + numCols + 1])) {
+                //utils.print("______________________bottom: {}\n", .{content[i + numCols]});
+                return true;
+            }
+        }
+        // top left char
+        if (i > numCols and i > 0) {
+            if (isSymbol(content[i - numCols - 1 .. i - numCols])) {
+                //utils.print("______________________top left: {}\n", .{content[i - numCols - 1]});
+                return true;
+            }
+        }
+        // top right char
+        if (i > numCols and i < content.len - 1) {
+            if (isSymbol(content[i - numCols + 1 .. i - numCols + 2])) {
+                //utils.print("______________________top right: {}\n", .{content[i - numCols + 1]});
+                return true;
+            }
+        }
+        // bottom left char
+        if (i < content.len - numCols and i > 0) {
+            if (isSymbol(content[i + numCols - 1 .. i + numCols])) {
+                //utils.print("______________________bottom left: {}\n", .{content[i + numCols - 1]});
+                return true;
+            }
+        }
+        // bottom right char
+        if (i < content.len - numCols - 1 and i < content.len - 2) {
+            if (isSymbol(content[i + numCols + 1 .. i + numCols + 2])) {
+                //utils.print("______________________bottom right: {}\n", .{content[i + numCols + 1]});
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+pub fn solve1(content: []const u8) !i32 {
+    var result: i32 = 0;
+    var numRows = std.mem.count(u8, content, "\n") + 1;
+    var numCols = ((content.len - numRows - 1) / numRows) + 1;
+    //utils.print("len: {}, num rows: {}, num cols: {}\n", .{ content.len, numRows, numCols });
+    var i = content.len;
+
+    while (i > 0) : (i -= 1) {
+        var pos = content.len - i;
+        if (std.ascii.isDigit(content[pos])) {
+            var new_i = i;
+            var n = getNumber(content[pos..], &new_i);
+            var hasSymbol = getHasSymbol(content, pos, content.len - new_i, numCols + 1);
+            //utils.print(">>>> {}, pos_start: {}, pos_end: {}, has symbol: {}\n", .{ n, pos, content.len - new_i - 1, hasSymbol });
+            if (hasSymbol) result += n;
+            i = new_i;
+        }
+        //utils.print("i: {}, pos: {}, char: {}, isDigit: {}\n", .{ i, pos, content[pos], std.ascii.isDigit(content[pos]) });
+    }
+
+    return result;
+}
+
+pub fn solve2(content: []const u8) !i32 {
+    _ = content;
+    return 0;
+}
+
+pub fn main() !void {
+    const content = @embedFile("data/day03.txt");
+    var result = try solve1(content);
+    utils.print("Result 1: {}\n", .{result});
+    //result = try solve2(content);
+    //utils.print("Result 2: {}\n", .{result});
+}
+
+test "part1 test" {
+    const content =
+        \\467..114..
+        \\...*......
+        \\..35..633.
+        \\......#...
+        \\617*......
+        \\.....+.58.
+        \\..592.....
+        \\......755.
+        \\...$.*....
+        \\.664.598..
+    ;
+    const result = try solve1(content);
+    try std.testing.expectEqual(@as(i32, 4361), result);
+}
+
+test "part2 test" {
+    const content =
+        \\467..114..
+        \\...*......
+        \\..35..633.
+        \\......#...
+        \\617*......
+        \\.....+.58.
+        \\..592.....
+        \\......755.
+        \\...$.*....
+        \\.664.598..
+    ;
+    const result = try solve2(content);
+    try std.testing.expectEqual(@as(i32, 467835), result);
+}
