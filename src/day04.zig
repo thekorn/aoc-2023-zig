7d8b79e70d90ac23eaca0c725aebf15f68f1ec36
Markus Korn <markus.korn@gmail.com>
2023-12-04 22:04:15 +0100

    feat(day04.zig): uncomment solve2 function and part2 test fix(day04.zig): modify solve2 function to return 0 as placeholder fix(day04.zig): update part2 test content to match new game rules This commit is to prepare for the implementation of the solve2 function.

--- src/day04.zig
+++ src/day04.zig
@@ -46,19 +46,10 @@ pub fn solve1(content: []const u8) !i32 {
     return result;
 }
 
-//pub fn solve2(content: []const u8) !i32 {
-//    var gameIter = std.mem.tokenize(u8, content, "\n");
-//    var currentGame: i32 = 1;
-//    var result: i32 = 0;
-//    while (gameIter.next()) |game| : (currentGame += 1) {
-//        var p = std.mem.indexOf(u8, game, ":");
-//        if (p == null) {
-//            return error.UnexpectedEof;
-//        }
-//        result += try getMinBoxCnt(game[p.? + 2 ..]);
-//    }
-//    return result;
-//}
+pub fn solve2(content: []const u8) !i32 {
+    _ = content;
+    return 0;
+}
 
 pub fn main() !void {
     const content = @embedFile("data/day04.txt");
@@ -81,14 +72,15 @@ test "part1 test" {
     try std.testing.expectEqual(@as(i32, 13), result);
 }
 
-//test "part2 test" {
-//    const content =
-//        \\Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-//        \\Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-//        \\Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-//        \\Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-//        \\Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-//    ;
-//    const result = try solve2(content);
-//    try std.testing.expectEqual(@as(i32, 2286), result);
-//}
+test "part2 test" {
+    const content =
+        \\Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        \\Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        \\Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        \\Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        \\Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        \\Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+    ;
+    const result = try solve2(content);
+    try std.testing.expectEqual(@as(i32, 30), result);
+}
